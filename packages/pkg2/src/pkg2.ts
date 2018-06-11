69ece4958f935b885826056b1205d589aaf7a1de
fkleuver <fred@avurad.nl>
2018-06-11 04:44:02 +0200

    chore(all): and let's introduce a little problem

--- packages/pkg2/src/pkg2.ts
+++ packages/pkg2/src/pkg2.ts
@@ -1,5 +1,5 @@
 import { Pkg1 } from 'pkg1';
 
 export class Pkg2 {
-  public pkg1: Pkg1;
+  public pkg1: Pkg1 = new Pkg1();
 }
--- packages/pkg3/src/pkg3.ts
+++ packages/pkg3/src/pkg3.ts
@@ -2,6 +2,6 @@ import { Pkg1 } from 'pkg1';
 import { Pkg2 } from 'pkg2';
 
 export class Pkg3 {
-  public pkg1: Pkg1;
-  public pkg2: Pkg2;
+  public pkg1: Pkg1 = new Pkg1();
+  public pkg2: Pkg2 = new Pkg2();
 }
